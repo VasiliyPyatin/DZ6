e4e0932a94c2f68bc44919aa469c7c44c5cd7e89
«Vasiliy» <pyatin_vs@mail.ru>
2022-07-07 01:49:50 +0300

    Задача 41 - решена

---
+++ task001/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите числа через пробел: ");
+int [] numbers = GetArrayFromString(ReadLine());
+WriteLine($"[{String.Join(", ",numbers)}] -> {CountNumbersAgainNull(numbers)}");
+
+
+int [] GetArrayFromString (string arrayStr)
+{
+    string [] arS=arrayStr.Split(new char[] {' ',',','*'},StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int [arS.Length];
+    for (int i=0; i<arS.Length; i++)
+    {
+        result[i]=int.Parse(arS[i]);
+    }
+    return result;
+}
+
+int CountNumbersAgainNull (int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i<arr.Length; i++)
+    {
+        if (arr[i]>0) count = count+1; 
+        
+    }
+    return count;
+}
